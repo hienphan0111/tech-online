c0d61e8969c8034e280a37d287c2e2c3c19eee96
hienphan0111 <hienphan@digitalcad.edu.vn>
2023-05-05 13:59:58 +0700

    Update product cart and products page

--- client/src/pages/products.jsx
+++ client/src/pages/products.jsx
@@ -1,14 +1,15 @@
 import { Center, Wrap, WrapItem } from '@chakra-ui/react';
 import { products } from '../products';
+import ProductCard from '../components/ProductCard';
 
 const ProductsPage = () => {
   return (
     <Wrap spacing='30px' justify='center' minHeight='100vh'>
       {
-        products.map(({ id, name}) => (
-          <WrapItem key={id}>
+        products.map((product) => (
+          <WrapItem key={product.id}>
             <Center w='250px' h='550px'>
-              {name}
+              <ProductCard product={product} />
             </Center>
           </WrapItem>
         ))
