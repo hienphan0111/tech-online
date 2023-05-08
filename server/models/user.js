f155c17c6ac8b80e5f9e3b62d8adf9f48f62ae9b
hienphan0111 <hienphan@digitalcad.edu.vn>
2023-05-08 20:54:58 +0700

    Add user model

---
+++ server/models/user.js
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+import { Await } from 'react-router-dom';
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+},
+  {
+    timestamps: true,
+  },
+);
+
+userSchema.methods.matchPasswords = async function(password) {
+  return await bcrypt.compare(password, this.password);
+}
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+
+  return next();
+})
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
